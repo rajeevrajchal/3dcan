031c8011ff60b02dce157e6c26e309446b6090db
Rajeev Rajchal <rajeevrajchal12@gmail.com>
2024-08-15 16:57:17 +0200

    Improvement: Added torusknot geometry with video texture

--- src/components/can/can-object.tsx
+++ src/components/can/can-object.tsx
@@ -2,13 +2,12 @@ import { useGLTF, useVideoTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
-import { $FIX_ME } from "../../types/fix_me";
 
 const CanObject = () => {
   const can = useGLTF("/can/scene.gltf");
   const videoTexture = useVideoTexture("/skate.mp4");
-  const canRef = useRef<$FIX_ME>();
-  const ringRef = useRef<$FIX_ME>();
+  const canRef = useRef<THREE.Group>(null);
+  const ringRef = useRef<THREE.Mesh>(null);
 
   useFrame(() => {
     if (canRef.current && ringRef.current) {
@@ -20,9 +19,14 @@ const CanObject = () => {
 
   return (
     <group>
-      <primitive object={can.scene} ref={canRef} />
-      <mesh ref={ringRef} rotation={[Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
-        <ringGeometry args={[0.5, 1, 32]} />
+      <primitive
+        object={can.scene}
+        ref={canRef}
+        scale={[4, 4, 4]}
+        position={[0, 0, 0]}
+      />
+      <mesh ref={ringRef} rotation={[Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+        <torusKnotGeometry args={[1, 0.05, 100, 10]} />{" "}
         <meshStandardMaterial map={videoTexture} side={THREE.DoubleSide} />
       </mesh>
     </group>
